4557e07ae65d0bbcdc43ba683247730fbd346427
Kyle Glaws <kyle.glaws@gmail.com>
2022-12-12 23:08:18 -0500

    finish 2022 day 11 (I had to cheat on part 2)

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,3 +1,11 @@
+import sys
+from typing import Callable
+
+# I cheated on part 2 for this one... Had absolutely no idea how
+# to deal with the huge numbers that were being made. Turns out
+# the answer is to multiply all the test mod values together to
+# produce a magic number, and use THAT as the mod for all monkeys.
+
 
 class Monkey:
 
@@ -43,27 +51,47 @@ def parse_monkeys(lines: list[str]) -> list[Monkey]:
     return [parse_monkey(paragraph.split("\n")) for paragraph in "\n".join(lines).split("\n\n")]
 
 
-def play_rounds(monkeys: list[Monkey], rounds: int):
+def get_magic_number(monkeys: list[Monkey]) -> int:
+    n = 1
+    for monkey in monkeys:
+        n *= monkey.test_operand
+    return n
+
+
+def play_rounds(monkeys: list[Monkey], worry_op: Callable[[int], int], rounds: int):
+    magic_number = get_magic_number(monkeys)
     for round_ in range(rounds):
-        print(round_)
+        #print(round_)
         for monkey in monkeys:
-            for i, item in enumerate(monkey.items):
+            for item in monkey.items:
+                #print(f"Monkey {monkey.monkey_number} inspecting {item}")
                 operand = item if monkey.inspect_operand == "old" else int(monkey.inspect_operand)
-                item = monkey.inspect_operation(item, operand) // 3
+                item = worry_op(monkey.inspect_operation(item, operand)) % magic_number
+                #print(f"new item value = {item}")
                 if item % monkey.test_operand == 0:
+                    #print(f"{item} tossed to monkey {monkey.true_action}")
                     monkeys[monkey.true_action].items.append(item)
                 else:
+                    #print(f"{item} tossed to monkey {monkey.false_action}")
                     monkeys[monkey.false_action].items.append(item)
                 monkey.inspections += 1
+            monkey.items = []
 
 
 if __name__ == "__main__":
     lines = []
-    with open("input.txt", "r") as infile:
+    with open("test.txt", "r") as infile:
         for line in infile.readlines():
             lines.append(line[:-1] if "\n" in line else line)
 
     monkeys = parse_monkeys(lines)
-    play_rounds(monkeys, 20)
-    print(sorted([monkey.inspections for monkey in monkeys]))
+
+    if len(sys.argv) > 1 and sys.argv[1] == "--part2":
+        play_rounds(monkeys, lambda n: n, 10000)
+    else:
+        play_rounds(monkeys, lambda n: n // 3, 20)
+
+    second, first = sorted([monkey.inspections for monkey in monkeys])[-2:]
+
+    print(f"Monkey business: {first} * {second} = {second * first}")
 
